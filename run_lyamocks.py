f9e728f3d95d48b88114b8585193a2fa13f0a2a5
francescosinigaglia <fsin_ext@iac.es>
2025-01-09 13:39:26 +0000

    uncommented files

--- run_lyamocks.py
+++ run_lyamocks.py
@@ -3,20 +3,20 @@ import os
 #module load python
 #export NUMBA_NUM_THREADS=256
 
-#os.system('cp auxarr/dcomOM0.314OL0.686.DAT .')
-#os.system('cp auxarr/zarr.DAT .')
+os.system('cp auxarr/dcomOM0.314OL0.686.DAT .')
+os.system('cp auxarr/zarr.DAT .')
 
 # QSO catalog                                                                                                                                                
-#os.system('python3 make_catalog_lightcone.py')
-#os.system('python3 write_catalog.py')
-#os.system('python3 rewrite_qso_cat_for_xcf.py')
+os.system('python3 make_catalog_lightcone.py')
+os.system('python3 write_catalog.py')
+os.system('python3 rewrite_qso_cat_for_xcf.py')
 
 # DLA catalog                                                                                                                                                
-#os.system('conda run -n pyigm python3 make_catalog_DLA.py')
-#os.system('python3 write_catalog_DLA.py')
+os.system('conda run -n pyigm python3 make_catalog_DLA.py')
+os.system('python3 write_catalog_DLA.py')
 
 # Lya catalog                                                                                                                                                
-#os.system('python3 make_mocks_cells.py')         #in all the other cases                 
+os.system('python3 make_mocks_cells.py')         #in all the other cases                 
 
 os.system('cp auxarr/dcomOM0.314OL0.686.DAT .')
 os.system('cp auxarr/zarr.DAT .')
